8fc3ae0b399d76db8ac1e639f5a7a9e65dd62747
Divya <divyazeliyan@gmail.com>
2022-07-12 10:20:42 +0530

    4th commit

---
+++ Countercart/work.js
@@ -0,0 +1,10 @@
+var countvalue=0
+function increment(){
+    countvalue=countvalue+1
+    document.getElementById("count").innerHTML=countvalue
+}
+function decrement(){
+    countvalue=countvalue-1
+    document.getElementById("count").innerHTML=countvalue
+
+}
